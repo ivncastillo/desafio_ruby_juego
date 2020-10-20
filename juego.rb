f42d46a4c411efcf431e7b1fe1f217a144f32f70
ivncastillo <ivncastillo@gmail.com>
2020-10-20 19:23:10 -0300

    segundo intento

--- juego.rb
+++ juego.rb
@@ -3,43 +3,46 @@ if humano != 'piedra' && humano != 'papel' && humano != 'tijera'
     print 'Argumento inválido: Debe ser piedra, papel o tijera.'
 else
     aleatorio=rand(3)
+
     if aleatorio==0
-        maquina='piedra'
-        if aletorio==1
-            maquina='papel'
-        else
-            maquina='tijera'
-        end
+        computador='piedra'
     end
-    print "Computador juega #{maquina}"
+    if aleatorio==1
+        computador='papel'
+    end
+    if aleatorio==2    
+        computador='tijera'
+    end
 
-    if humano=='piedra' && computador='piedra'
+    puts "Computador juega #{computador}" 
+
+    if humano=='piedra' && computador=='piedra'
         print "Empataste"
     end
-    if humano=='piedra' && computador='papel'
+    if humano=='piedra' && computador=='papel'
         print "Perdiste"
     end
-    if humano=='piedra' && computador='tijera'
+    if humano=='piedra' && computador=='tijera'
         print "Ganaste"
     end
 
-    if humano=='papel' && computador='piedra'
+    if humano=='papel' && computador=='piedra'
         print "Ganaste"
     end
-    if humano=='papel' && computador='papel'
+    if humano=='papel' && computador=='papel'
         print "Empataste"
     end
-    if humano=='papel' && computador='tijera'
+    if humano=='papel' && computador=='tijera'
         print "Perdiste"
     end
 
-    if humano=='tijera' && computador='piedra'
+    if humano=='tijera' && computador=='piedra'
         print "Perdiste"
     end
-    if humano=='tijera' && computador='papel'
+    if humano=='tijera' && computador=='papel'
         print "Ganaste"
     end
-    if humano=='tijera' && computador='tijera'
+    if humano=='tijera' && computador=='tijera'
         print "Empataste"
     end
 
